08ee3e4a7ef399d3b4352f6cd7d17bf10858cbe0
Thedorus89 <theodoredanang@gmail.com>
2024-08-12 11:05:06 +0700

    End of section 9

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/layout/styles.css'
-import 'semantic-ui-css/semantic.min.css'
-import { store, StoreContext } from './app/stores/store'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './app/router/Routes'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './app/layout/styles.css';
+import 'semantic-ui-css/semantic.min.css';
+import 'react-calendar/dist/Calendar.css';
+import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
